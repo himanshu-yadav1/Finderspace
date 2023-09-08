876e533bf79c3814df1eb6d3e5c952194fdd35b8
Himanshu <himanshuydv7777@gmail.com>
2023-09-08 14:17:40 +0530

    Navbar added

--- finderspace/src/App.js
+++ finderspace/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import About from './Components/About';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar/>
+
+        <div className="container my-3">
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} /> 
+          </Routes>    
+
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
