371225f63d1fad171c452c914a98bd5607020830
kartikgoyal89 <kartikgoyal794@gmail.com>
2023-09-09 19:52:09 +0530

    changed the ui

--- finderspace/src/Components/University.js
+++ finderspace/src/Components/University.js
@@ -1,36 +1,85 @@
-import React from 'react'
-import image from './university2_image.jpg'
-import enggimage from './Engg_image.jpg'
-import medicalimage from './Medical_image.png'
-import bbaimage from './BBA_image.jpg'
-import mbaimage from './MBA_image.jpg'
-import './University.css'
-import Courses from './Courses'
+import React from "react";
+import image from "./university2_image.jpg";
+import enggimage from "./Engg_image.jpg";
+import medicalimage from "./Medical_image.png";
+import bbaimage from "./BBA_image.jpg";
+import mbaimage from "./MBA_image.jpg";
+import avatar from "./avatar.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse,faCircle,faMessage,faBook,faBuildingColumns,faGear,faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import Courses from "./Courses";
+import "./University.css";
 
 const University = () => {
-    return (
-        <div className="row">
-            <div className="col">
-                1
-            </div>
-            <div className="col-9">
-                <div id="university-name">
-                    <h1>University Name</h1>
-                </div>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={image} className="card-img-top university-image" alt="..." />
-                </div>
-
-                <div className="courses">
-                    <h2>Courses</h2>
-                    <Courses title="Engineering" image={enggimage}/>
-                    <Courses title="Medical" image={medicalimage}/>
-                    <Courses title="BBA" image={bbaimage}/>
-                    <Courses title="MBA" image={mbaimage}/>
-                </div>
-            </div>
+  return (
+    <div className="row">
+      <div className="col">
+        <div className="user-con">
+          <img src={avatar} alt="" />
+          <div className="text">
+            <h2>User</h2>
+          </div>
         </div>
-    )
-}
+        <ul className="features">
+          <div className="feature">
+            <FontAwesomeIcon icon={faHouse} />
+            <li>Dashboard</li>
+          </div>
 
-export default University
+          <div className='feature'>
+          <FontAwesomeIcon icon={faCircle} />
+            <li>About</li>
+          </div>
+          <div className='feature'>
+          <FontAwesomeIcon icon={faMessage} />
+            <li>Contact</li>
+          </div>
+          <div className='feature'>
+          <FontAwesomeIcon icon={faBook} />
+            <li>Courses</li>
+          </div>
+          <div className='feature'>
+          <FontAwesomeIcon icon={faBuildingColumns} />
+            <li>University</li>
+          </div>
+          <div className='feature'>
+          <FontAwesomeIcon icon={faGear} />
+            <li>Settings</li>
+          </div>
+        </ul>
+      </div>
+      <div className="col-9">
+        <div className="university">
+          <div id="university-name">
+            <h1>University of Queensland</h1>
+            <FontAwesomeIcon className='location-icon' icon={faLocationDot} />
+            <p className='location'>Australia</p>
+            <p className="details"></p>500m walk to metro . Close to market . Best College in Australia <br /> 1000+ Alumnis placed in Top MNCs
+            <br />
+            <button className='explore'>Explore Now</button>
+          </div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src={image}
+              className="card-img-top university-image"
+              alt="..."
+            />
+          </div>
+          <div className="courses course-list">
+            <h2>Courses</h2>
+            <div className="course-1">
+              <Courses title="Engineering" image={enggimage} />
+              <Courses title="Medical" image={medicalimage} />
+            </div>
+            <div className="course-2">
+              <Courses title="BBA" image={bbaimage} />
+              <Courses title="MBA" image={mbaimage} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default University;
--- finderspace/src/index.js
+++ finderspace/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
