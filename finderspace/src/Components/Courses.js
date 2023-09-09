51a71e497ce538a9df11b85d5dd944c97c79ed74
Himanshu <himanshuydv7777@gmail.com>
2023-09-09 20:25:20 +0530

    Warnings fixed

--- finderspace/src/Components/Courses.js
+++ finderspace/src/Components/Courses.js
@@ -5,7 +5,7 @@ const Courses = (props) => {
     return (
         <div>
             <div class="card" style={{ width: "11rem" }}>
-                <img class="card-img-top courses" src={props.image} alt="Card image cap" />
+                <img class="card-img-top courses" src={props.image} alt="Card cap" />
                 <div class="card-body">
                     <h4 class="card-title">{props.title}</h4>
                 </div>
--- finderspace/src/Components/Navbar.js
+++ finderspace/src/Components/Navbar.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    let location = useLocation();
 
     return (
         <div>
